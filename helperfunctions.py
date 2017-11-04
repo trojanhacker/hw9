f6096f2b2ee5bfeeded01a569d6b45da0c783452
trojanhacker <henrychien16@hotmail.com>
2017-11-03 18:18:25 -0700

    Add files via upload

---
+++ helperfunctions.py
@@ -0,0 +1,18 @@
+# helperfunction.py
+import math;
+
+def add (x,y):
+    return (x+y);
+
+def diff (x,y):
+    return (math.fabs(x-y));
+
+def product (x,y):
+    return (x*y);
+
+def greatest (x,y):
+    if (x > y):
+        return x;
+    else:
+        return y;
+
